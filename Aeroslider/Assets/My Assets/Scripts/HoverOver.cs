2ad3e56b2b59a6c17c755e83e4b62c8e5b8cd828
Gavin Carroll <gavcarroll98@gmail.com>
2019-10-16 13:48:21 -0400

    Menu Update

--- Aeroslider/Assets/My Assets/Scripts/HoverOver.cs
+++ Aeroslider/Assets/My Assets/Scripts/HoverOver.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoverOver : MonoBehaviour
-{
+public class HoverOver : MonoBehaviour    {
+
+    public Material HighlightMaterial;
+    private Material savedMaterial;
+    private GameObject curObj;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       // renderer.material.color = Color.white;
+    }
+
+    void OnMouseEnter()
+    {
+        //GetComponent<Renderer>().material.color = Color.blue;
+        //savedMaterial = curObj.GetComponent<Renderer>().material;
+        //curObj.GetComponent<Renderer>().material = HighlightMaterial;
+    }
+
+    void OnMouseExit()
+    {
+        //GetComponent<Renderer>().material.color = Color.white; 
+       // curObj.GetComponent<Renderer>().material = savedMaterial;
     }
 
     // Update is called once per frame
---
+++ Aeroslider/Assets/My Assets/Scripts/PointerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerControl : MonoBehaviour
+{
+    private Vector3 mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = 5f; // Set this to be the distance you want the object to be placed in front of the camera.
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+}
